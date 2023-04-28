96bb85ff62ff4541aa5c7376e4f9ff31d1423232
Forth <33458877+Forthtilliath@users.noreply.github.com>
2023-04-28 12:24:53 +0200

    footer section

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -1,3 +1,3 @@
 export function Footer() {
-    return <footer class="h-20 bg-orange-300">Footer</footer>
+    return <footer class="bg-ori-black text-white text-center p-2 pt-10">© Codé par Forth - Concours #6 : Créer un site pour un Restaurant / Café / Bar</footer>
 }
